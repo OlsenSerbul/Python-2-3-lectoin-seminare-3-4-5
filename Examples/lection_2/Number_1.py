0e31798c08e386fb0b5f7e75246448adafbeb5f3
Olga Serbul <Olsen.zika@mail.ru>
2023-01-24 10:45:24 +0300

    lection_2

--- Examples/lection_2/Number_1.py
+++ Examples/lection_2/Number_1.py
@@ -1,6 +1,23 @@
-# Создаем список 
-colors = ['red','green','blue']
-# создаем переменную 
-data = open('file.txt', 'a')
-data.writelines(colors)
-data.close()
+# # Создаем список 
+# colors = ['red','green','blue123']
+# # # создаем переменную 
+# data = open('file.txt', 'a')
+# data.writelines(colors)
+# # data.write('LINE 11221\n')
+# # data.write('LINE 11331\n')
+# # data.close()
+
+
+#или записываем по другому
+
+# with open('file.txt', 'a') as data:
+#     data.write('line 1\n')
+#     data.write('line 2\n')
+
+hw_set=[1,3,8,9,10,15,16,16,'my_string','your_string']
+hw_set_copy = hw_set.copy()
+print(hw_set_copy)
+for el in range(len(hw_set)): 
+    if el!= int(el):
+        hw_set.remove(el)
+print(hw_set)
