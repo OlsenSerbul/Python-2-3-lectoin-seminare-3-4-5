c4a5332b0f28410ab9023db76a78a8d8d1f3da70
Olga Serbul <Olsen.zika@mail.ru>
2023-01-26 23:59:25 +0300

    домашнее_работа_3_задача_2

--- Examples/Домашняя_работа_3/2.py
+++ Examples/Домашняя_работа_3/2.py
@@ -4,3 +4,21 @@
 # [2, 3, 4, 5, 6] => [12, 15, 16];
 # [2, 3, 5, 6] => [12, 15]
 
+import random
+
+num = int(input('Введите число элементов списка: '))
+my_list = []
+for i in range(num):
+    my_list.append(random.randint(1, 100))
+print(f'Первоначальный список:  {my_list}')
+
+new_list = []
+if num % 2 == 0:
+   for i in range(int(num / 2 )):
+       new_list.append(my_list[i] * my_list[num -1 - i])
+else:
+    for i in range(int(num / 2 + 1 )):
+        new_list.append(my_list[i] * my_list[num -1 - i])
+
+print(f'Результатом будет список: {new_list} ')
+
