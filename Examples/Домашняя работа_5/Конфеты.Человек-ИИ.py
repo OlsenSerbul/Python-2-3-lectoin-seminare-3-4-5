45de9682f3c621217069394203108f0c611a4a9b
Olga Serbul <Olsen.zika@mail.ru>
2023-02-02 12:55:27 +0300

    домашняя_работа_5_конфеты.человек-ии

--- Examples/Домашняя работа_5/Конфеты.Человек-ИИ.py
+++ Examples/Домашняя работа_5/Конфеты.Человек-ИИ.py
@@ -1,6 +1,6 @@
 import random
 num_sweet = random.randint(100, 200)
-#print(num_sweet)
+print(num_sweet)
 print('Привет! Меня зовут Max. Давай сыграем в игру! \n '
      'На столе лежит неизвестное число конфет: от 100 до 200.\n'
      'Поочереди каждый из нас берет любое количество конфет, но НЕ БОЛЕЕ 28 штук. Победителем считается тот \n'
@@ -13,12 +13,20 @@ if play == 1:
     print(f'Жеребьевка закончена. Сейчас ход игрока {player_list[0]}')
     while num_sweet >= 0:
          print('Конфеты есть, играем')
-         if player_list[0] == 'Max':
-            took_sweet = random.randint(1, 29)
-            print(f'Я беру {took_sweet} конфет')
-            num_sweet -= took_sweet
-            if num_sweet > 0:
-               player_list[0] = 'Человек'
+         while player_list[0] == 'Max':
+            if num_sweet < 56 and num_sweet > 35:
+                took_sweet = 1
+                print(f'Я беру {took_sweet} конфет')
+                num_sweet -= took_sweet
+                player_list[0] = 'Человек'
+
+            else:
+                took_sweet = 28
+                print(f'Я беру {took_sweet} конфет')
+                num_sweet -= took_sweet
+                if num_sweet > 0:
+                   player_list[0] = 'Человек'
+            break
 
 
          else:
@@ -32,10 +40,3 @@ if play == 1:
     print(f'Конфет больше нет!Победил {player_list[0]}!') 
 else:
      print('Может в следующий раз. Пока!')   
-            
-            
-
-
-
-
-
